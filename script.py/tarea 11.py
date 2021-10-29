c1cbcb992f52b5f54da69692d4ca78f0957f6c9a
andmansim <91722324+andmansim@users.noreply.github.com>
2021-10-29 23:49:14 +0200

    terminado

--- script.py/tarea 11.py
+++ script.py/tarea 11.py
@@ -1,6 +1,6 @@
 import random 
 import seaborn as sns
-import matplotlib.pyplot as ply
+import matplotlib.pyplot as plt
 
 iterations = 10000
 variables =['work', 'salary', 'conditions', 'career_advancement', 'social_impact', 'job_security', 'life_balance', 'location', 'atmosphere', 'travel']
@@ -16,14 +16,14 @@ def Monte_Carlo(grade):
         final_results.append(sum(results))
     return final_results
 
-a= Monte_Carlo([[4,9], [8.5,10], [5,9], [8.5,9.5], [3,7], [4,9], [3,8], [7.5,8], [5,9], [0,6]])
+a = Monte_Carlo([[4,9], [8.5,10], [5,9], [8.5,9.5], [3,7], [4,9], [3,8], [7.5,8], [5,9], [0,6]])
 b = Monte_Carlo([[5,10], [4,4], [7,9],[2,8], [6,9.5], [8.5,10], [8,10], [0,7], [3,9], [0,3]])
 c = Monte_Carlo([[4,7], [6,8], [6,9],[6.5,9], [2,6], [6.5,9], [5.5,9], [9.5,9.5], [5,9], [4, 9]])
 
 fig = plt.figure(figsize=(10,6))
 sns.displot(a)
 sns.displot(b)
-sns.displot(c);
-ptl.legend(labels=['Job A', 'Job B', 'Job C'])
+sns.displot(c)
+plt.legend(labels=['Job A', 'Job B', 'Job C'])
 plt.title('Monte-Carlo Distributions')
 plt.show()
